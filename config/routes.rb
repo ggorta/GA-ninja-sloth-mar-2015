2a7aa08c7e01f8abde9e7d658485f4238e8a6082
Andres Valencia <andresv2@mac.com>
2015-03-18 20:25:18 -0400

    adds sessions route file

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,20 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  root 'sessions#new'
 
   resources :search
+
+
+
+  #sessions
+ get "log_out" => "sessions#destroy", :as => "log_out"
+get "log_in" => "sessions#new", :as => "log_in"
+get "sign_up" => "users#new", :as => "sign_up"
+root :to => "users#new"
+resources :users
+resources :sessions
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
